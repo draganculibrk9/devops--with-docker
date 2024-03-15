b29e66b5785d634d0c34bf1bac235b01501979a3
draganculibrk9 <d.culibrk@vegait.rs>
2024-03-15 14:17:44 +0100

    feat: Complete exercise 3.3

---
+++ 3/2/builder.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if both arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 GITHUB_REPOSITORY DOCKER_HUB_REPOSITORY"
+    exit 1
+fi
+
+# Assign arguments to variables
+GITHUB_REPOSITORY=$1
+DOCKER_HUB_REPOSITORY=$2
+
+GITHUB_URL="https://github.com/$GITHUB_REPOSITORY"
+
+# Clone the GitHub repository
+git clone $GITHUB_URL $GITHUB_REPOSITORY
+
+# Check if clone was successful
+if [ $? -eq 0 ]; then
+    echo "Repository cloned successfully"
+else
+    echo "Failed to clone repository"
+    exit 1
+fi
+
+# Build and tag image
+cd $GITHUB_REPOSITORY && docker build -t $DOCKER_HUB_REPOSITORY .
+
+# Push the image to Docker Hub
+docker push $DOCKER_HUB_REPOSITORY
